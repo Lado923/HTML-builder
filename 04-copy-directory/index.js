2747f2b68f677b003201a9bebd2d930b03ee96cb
Lado923 <slava923@gmail.com>
2022-05-24 01:28:17 +0300

    feat: add solution for 04-task LANG=en_EN date +'(%a, %b %d, %Y %r)'"

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const path = require('path');
+const fs = require('fs');
+
+const promises = require('fs/promises');
+
+
+async function copyDir(params) {
+
+    await fs.promises.rm(path.join(__dirname, 'files-copy'), {force: true, recursive: true });
+
+    await fs.promises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+
+    const pathOrigin = path.join(__dirname, 'files');
+
+    const filesOrigin = await fs.promises.readdir(pathOrigin, {withFileTypes: true});
+
+    for (let item of filesOrigin) {   
+        
+        const fileOrigin = path.join(__dirname, 'files', item.name);
+        const fileCopy = path.join(__dirname, 'files-copy', item.name);
+        
+        await fs.promises.copyFile(fileOrigin, fileCopy);
+    };
+
+};
+
+copyDir()
 
